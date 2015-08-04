003df15a70f0cc35b6eba99bdc35588fb6dbd847
dbatalha <daniel.p.batalha@sapo.pt>
2015-08-04 22:06:20 +0100

    Create class DuckParser.

--- getwebdata.py
+++ getwebdata.py
@@ -3,57 +3,60 @@ from selenium.webdriver.common.keys import Keys
 from duck_html_parser import *
 import os
 
-def main():
-    local_dir = os.getcwd()
-    csv_dir = os.path.join(local_dir, 'words_data')
 
-    driver = webdriver.Firefox()
-    driver.get("https://duckduckgo.com/")
+class DuckParser(object):
 
-    for csv in os.listdir(csv_dir):
-        if csv.endswith(".csv"):
+    def __init__(self):
+        self.local_dir = os.getcwd()
+        self.csv_dir = os.path.join(self.local_dir, "words_data")
 
-            current_file_path = os.path.join(csv_dir, csv)
-            words_file = open(current_file_path, "r")
+        self.driver = webdriver.Firefox()
 
-            for word in words_file:
-                prep = word.strip()
-                driver.find_element_by_id('search_form_input_homepage').send_keys(prep)
-                driver.find_element_by_id('search_button_homepage').click()
+    def get_duck_websites(self):
+        self.driver.get("https://duckduckgo.com/")
 
-                for times in range(0, 50):
-                    driver.find_element_by_id('links_wrapper').send_keys(Keys.SPACE)
+        for csv in os.listdir(self.csv_dir):
+            if csv.endswith(".csv"):
 
-                html_source = driver.page_source
+                current_file_path = os.path.join(self.csv_dir, csv)
+                words_file = open(current_file_path, "r")
 
-                parser = GoDuckParser()
-                parser.feed(html_source)
-                html_data = parser.get_data()
+                for word in words_file:
+                    prep = word.strip()
+                    self.driver.find_element_by_id('search_form_input_homepage').send_keys(prep)
+                    self.driver.find_element_by_id('search_button_homepage').click()
 
-                html = html_data.split()
-                lenhtml = len(html)
+                    for times in range(0, 50):
+                        self.driver.find_element_by_id('links_wrapper').send_keys(Keys.SPACE)
 
-                domains = [".net", ".com", ".org", ".edu"]
-                for domain in domains:
-                    i = 0
-                    while i < lenhtml:
-                        net = html[i].find(str(domain))
-                        if int(net) >= 0:
-                            dots = html[i].find("...")
-                            more = html[i].find("More")
-                            if int(dots) != -1 and int(more) != -1:
-                                complete_url = html[i][int(dots)+3:int(more)]
-                                garbage = complete_url.find(")")
-                                random_dot = complete_url.find(".")
-                                if int(garbage) == 0:
-                                    print html[i][int(garbage)+2:int(dots)]
-                                if int(random_dot) == 0:
-                                    print "Random dot here"
-                                print complete_url
-                        i += 1
+                    html_source = self.driver.page_source
 
-                driver.get("https://duckduckgo.com/")
+                    parser = GoDuckParser()
+                    parser.feed(html_source)
+                    html_data = parser.get_data()
 
-        driver.close()
-if __name__ == "__main__":
-    main()
+                    html = html_data.split()
+                    lenhtml = len(html)
+
+                    domains = [".net", ".com", ".org", ".edu"]
+                    for domain in domains:
+                        i = 0
+                        while i < lenhtml:
+                            net = html[i].find(str(domain))
+                            if int(net) >= 0:
+                                dots = html[i].find("...")
+                                more = html[i].find("More")
+                                if int(dots) != -1 and int(more) != -1:
+                                    complete_url = html[i][int(dots)+3:int(more)]
+                                    garbage = complete_url.find(")")
+                                    random_dot = complete_url.find(".")
+                                    if int(garbage) == 0:
+                                        print html[i][int(garbage)+2:int(dots)]
+                                    if int(random_dot) == 0:
+                                        print "Random dot here"
+                                    print complete_url
+                            i += 1
+
+                    self.driver.get("https://duckduckgo.com/")
+
+            self.driver.close()
