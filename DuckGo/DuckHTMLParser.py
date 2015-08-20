1b2e49c868fb2919cc99827bfa95819245407acf
dbatalha <daniel.p.batalha@sapo.pt>
2015-08-20 22:02:05 +0100

    Fix typo on documentation.

--- DuckGo/DuckHTMLParser.py
+++ DuckGo/DuckHTMLParser.py
@@ -1,13 +1,14 @@
 from HTMLParser import HTMLParser
 
+
 class GoDuckParser(HTMLParser):
-    '''
+    """
     def handle_starttag(self, tag, attrs):
         print "Encounter a start tag:", tag
 
     def handle_endtag(self, tag):
         print "Encounter a end tag:", tag
-    '''
+    """
     def __init__(self):
         self.reset()
         self.fed = []
