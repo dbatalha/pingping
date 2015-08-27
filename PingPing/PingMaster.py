4402e2eda96ef3d1dd35997df11fcf49e92c413c
dbatalha <daniel.p.batalha@sapo.pt>
2015-08-27 22:33:06 +0100

    Add function for process data from ping command in Linux.

--- PingPing/PingMaster.py
+++ PingPing/PingMaster.py
@@ -4,7 +4,31 @@ import subprocess
 
 class PingMaster(object):
     def __init__(self):
-        self.ip_address = None
+        # Used on ping_web
+        self.ping_raw_data = None
+
+        # Used on method get_ip_address
+        self.address_ping = None
+        self.ip_ping = None
+
+    def get_ip_address(self):
+        """
+        This method get the data from website ip address and address from ping command.
+        :return:
+        """
+
+        # Process string from ping subprocess command, string address name.
+        ifs_address_1 = str(self.ping_raw_data).find("PING")
+        ifs_address_2 = str(self.ping_raw_data).find("(")
+
+        # Process string get ip address.
+        ifs_ip_1 = str(self.ping_raw_data).find(")")
+
+        # Assemble the variables for ip and address.
+        self.address_ping = str(self.ping_raw_data)[ifs_address_1 + 5:ifs_address_2]
+        self.ip_ping = str(self.ping_raw_data)[ifs_address_2 + 1:ifs_ip_1]
+
+        print "Address: %s IP: %s" % (self.address_ping, self.ip_ping)
 
     def ping_web(self, address):
         """
@@ -12,5 +36,8 @@ class PingMaster(object):
         :param address:
         :return:
         """
-        print self.ip_address
-        subprocess.call(["ping", "-c", "1", address])
+
+        process = subprocess.Popen(["ping", "-c", "1", address], stdout=subprocess.PIPE)
+        self.ping_raw_data, error = process.communicate()
+
+        self.get_ip_address()
